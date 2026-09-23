cf467ca558ad5f706666550b9cd5b4135c76ce2a
Prakash Jayavanth Mandi <prakashmandi24bcs86@iiitkottayam.ac.in>
2026-09-23 13:13:48 +0530

    chore: migrate from honcho to start.sh for render deployment

---
+++ backend/start.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+set -e
+
+uvicorn app.main:app --host 0.0.0.0 --port "$PORT" &
+API_PID=$!
+
+celery -A app.workers.celery_app worker --loglevel=info &
+WORKER_PID=$!
+
+celery -A app.workers.celery_app beat --loglevel=info &
+BEAT_PID=$!
+
+trap "kill $API_PID $WORKER_PID $BEAT_PID" SIGTERM SIGINT
+
+wait -n $API_PID $WORKER_PID $BEAT_PID
